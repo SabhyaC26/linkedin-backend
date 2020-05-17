ea20046cc792ac8783173d184741b100cb425ead
ssc255 <ssc255@cornell.edu>
2020-05-17 01:01:45 -0700

    gql complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 (async () => {
     const app = express();
@@ -7,6 +8,21 @@ import express from "express";
     app.get("/", (_req, res) => {
         res.send("hello");
     });
+
+    const apolloServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                hello: String!
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => "hello world :)"
+            }
+        }
+    });
+
+    apolloServer.applyMiddleware({ app });
 
     const port: number = 8000;
     app.listen(port, () => {
